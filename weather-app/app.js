5064d8138a08b5530598c01cc99e3880bdfb7d5b
StingRei26 <Reinielravelo@gmail.com>
2021-02-23 10:32:54 -0500

    update

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,6 @@
-//http://api.weatherstack.com/current?access_key=6b4b984cc50d04840187eb3f416ccb4a&query=37.8267,-122.4233
-
-
 const request = require('request')
+const geocode = require('./utils/geocode')
+
 
 // const url = 'http://api.weatherstack.com/current?access_key=6b4b984cc50d04840187eb3f416ccb4a&query=37.8267,-122.4233&units=f';
 // request({ url: url, json: true }, (error, response) => {
@@ -17,18 +16,41 @@ const request = require('request')
 //    }  
 // })
 
-// const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmVpbmllbCIsImEiOiJja2xhNmFyOWgydmxkMm90N3kyajZxankwIn0.nn1AtE04aDInWrEF12t4Hw&limit=1'
-// request({ url: geocodeURL, json: true }, (error, response) => {
 
-// if (error) {
-//     console.log("Unable to connect to location service")
-// } else if (response.body.features.length === 0){
-//     console.log("Unable to find location. Try another search."); 
-// } else {
-//     const latitude = response.body.features[0].center[0];
-//     const longitue = response.body.features[0].center[1];
-//     console.log(latitude, longitue);
-//  }
-// })
+geocode('New York', (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+})
 
-cosnt 
+
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+
+const forecast = (weather, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=6b4b984cc50d04840187eb3f416ccb4a&query=37.8267,-122.4233&units=f';
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback("Unable to connect to weather service");
+        } else if (response.body.error) {
+            callback("Unable to find location")
+        } else {
+            callback(undefined, {
+                console.log(response.body.current.weather_descriptions[0] + ". It is currently" + " " + body.temperature + " " + "degress out. It feels like 5 out.")
+            })
+        }
+    })
+}
+
+
+forecast(-75.7088, 44.1545, (error, data) => {
+    console.log('Error', error)
+    console.log('Data', data)
+});
