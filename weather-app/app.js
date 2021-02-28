36a859e504b6e3d39d8f6086082038c2cb60dfa7
StingRei26 <Reinielravelo@gmail.com>
2021-02-28 11:23:34 -0500

    6-raw-http.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -6,15 +6,15 @@ const address = process.argv[2]
 if (!address) {
     console.log("Please provide an address")
 } else {
-    geocode(address, (error, data) => {
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
         if (error) {
             return console.log(errror)
         }  
-        forecast(data.latitude, data.longitude, (error, forcastData) => {
+        forecast(latitude, longitude, (error, forcastData) => {
             if (error) {
                 return console.log(error);
             }
-            console.log(data.location)
+            console.log(location)
             console.log(forcastData);
         })
     })
