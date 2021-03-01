b9cf11d5be3270bf81271a7f82b2f6e25640de31
StingRei26 <Reinielravelo@gmail.com>
2021-03-01 10:42:05 -0500

    epress update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,1 +1,31 @@
 const express = require('express')
+
+
+const app = express(); 
+
+
+// app.com
+// app.com/help
+// app.com/about
+
+app.get('', (req, res) => {
+    res.send('Hello express')
+})
+
+app.get('/help', (req, res) => {
+    res.send('Help page')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About my app')
+})
+
+app.get('/weather', (req, res) => {
+    res.send('This is the weather')
+})
+
+// To start server on port 3000
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000.')
+})
+
