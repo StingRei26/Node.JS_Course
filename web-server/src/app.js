ab35d5ec785eaa9e79a9cdbaed76c56381df80d4
StingRei26 <Reinielravelo@gmail.com>
2021-03-04 10:00:49 -0500

    Using Handlebars
    dynamic file (index,about,help)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,33 @@
 const path = require('path');
-const { request } = require('express');
 const express = require('express')
 
-
-const app = express(); 
-
+const app = express() 
 const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath))
 
+// Using handlebars to serv dynamic html files (index.hbs)
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather', 
+        name: 'Rei'
+    })
+})
+// Using handlebars to serv dynamic html files (about.hbs)
+app.get('/about', (req, res) => {
+    res.render('about', {
+       title: 'About Me', 
+       name: 'Rei Ravelo'
+    })
+})
 
+app.get('/help', (req, res) => {
+    res.render('help', {
+     title: 'Help',
+     message: 'This is the massage'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
