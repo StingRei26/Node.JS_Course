a0d7dbcfabf81e95ad0259beb31801d783c7316e
StingRei26 <Reinielravelo@gmail.com>
2021-08-18 19:08:46 -0400

    update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const express = require('express')
 const hbs = require('hbs');
-// const { timeStamp } = require('console');
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+
 
 const app = express() 
 
@@ -45,37 +45,32 @@ app.get('/help', (req, res) => {
 })
 
 // This leads to a .json files via 'send'
+
 app.get('/weather', (req, res) => {
-    
     if (!req.query.address) {
         return res.send({
-            error: 'You must provide an address'
+            error: 'You must provide an address!'
         })
     }
 
-
-    geocode(req.query.address, (error, {latitude, longitude, location}) => {
-    
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
         if (error) {
-            return res.send({error})
+            return res.send({ error })
         }
-     
-    forecast(latitude, longitude, (error, forecastData) => {
-        if (error) {
-            return res.send({error})
-        }
-          res.send({
-              forecast: forecastData, 
-              location, 
-              address: req.query.address
-          })
-    
-       })
 
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
-
 })
-
 
 
 // 
