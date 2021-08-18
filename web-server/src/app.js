0133ad6e55b5cc122e31abd2c3536dcb0aec55d4
StingRei26 <Reinielravelo@gmail.com>
2021-08-18 18:30:17 -0400

    update

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const express = require('express')
 const hbs = require('hbs');
-const { timeStamp } = require('console');
+// const { timeStamp } = require('console');
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express() 
 
@@ -50,13 +52,31 @@ app.get('/weather', (req, res) => {
             error: 'You must provide an address'
         })
     }
-    res.send({
-        forcast:'It is Nice and Sunny',
-        location:'New Jersey',
-        address:req.query.address
+
+
+    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+    
+        if (error) {
+            return res.send({error})
+        }
+     
+    forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+            return res.send({error})
+        }
+          res.send({
+              forecast: forecastData, 
+              location, 
+              address: req.query.address
+          })
+    
+       })
 
     })
+
 })
+
+
 
 // 
 app.get('/products', (req, res) =>{
