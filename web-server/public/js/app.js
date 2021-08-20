438f1e89d3b06988ce4e45fe67eec8b7dc6064d6
StingRei26 <Reinielravelo@gmail.com>
2021-08-20 12:46:42 -0400

    update

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,14 +1,28 @@
 // to fetch json data from a URL and parse it w weather info 
-fetch('http://localhost:3000/weather?address=new jersey').then((response) =>{
+const weatherForm = document.querySelector('form'); 
+const search = document.querySelector('input'); 
+const messageOne = document.querySelector('#message-1');
+const messageTwo = document.querySelector('#message-2');
+
+weatherForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('http://localhost:3000/weather?address=' + location).then((response) =>{
         response.json().then((data) => {
     //    checking for errors first
      if (data.error) {
-         console.log(data.error)
+        messageOne.textContent = data.error
      } else {
-        console.log(data.location)
-        console.log(data.forecast)
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
      }
 
     })
    
+  })
+// console.log(location)
 })
